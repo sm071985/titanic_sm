16cf19cdac49c86d8a6b0bbfcb8bd380affc0645
sm071985 <sm071985@gmail.com>
2025-02-18 18:39:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,107 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle as pkl
+from pages.test import test_page
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+from sklearn.model_selection import train_test_split,cross_val_score
+from sklearn.svm import SVC,LinearSVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+st.title("Training")
+def age_band(df):
+    df.loc[df['Age'] <= 16, 'AgeBand'] = '0 - 16'
+    df.loc[(df['Age'] > 16) & (df['Age'] <= 32), 'AgeBand'] = '17 - 32'
+    df.loc[(df['Age'] > 32) & (df['Age'] <= 48), 'AgeBand'] =   '33 - 48'
+    df.loc[(df['Age'] > 48) & (df['Age'] <= 64), 'AgeBand'] = '49 - 64'
+    df.loc[(df['Age'] > 64),'AgeBand'] = '65 Above'
+    return df
+
+feature_dict = {}
+def cata2lbl(df, feature, values):
+    for value in values:
+        # st.write(feature_dict[feature] )
+        if feature+'_l' not in feature_dict.keys():
+            feature_dict[feature+'_l'] = {value.upper(): values.index(value)}
+        else:
+            feature_dict[feature+'_l'].update({value.upper(): values.index(value)})
+        df.loc[(df[feature] == value), feature+'_l'] = values.index(value)
+    with open('./models/Features.pkl', 'wb') as f:
+        pkl.dump(feature_dict, f)
+    # st.write(feature_dict,)
+    return df
+
+@st.cache_data
+def load_data():
+    train = pd.read_csv("./dataset/train.csv")
+    train = train.dropna()
+    train = age_band(train)
+    train = train.drop(columns = ['Age'])
+
+    train = train.drop(columns = ['PassengerId', 'Pclass', 'Name','SibSp', 'Parch', 'Ticket', 'Fare','Cabin', ])
+
+    # test = test.drop(columns = ['PassengerId', 'Pclass', 'Name','SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin' ])
+
+    Y_train = train['Survived']
+
+    X_train = train.drop(columns = ['Survived'])
+
+    # st.write(X_train)
+    for feature in list(X_train.select_dtypes(include=[object])):
+        X_train = cata2lbl(X_train, feature, sorted(list(X_train[feature].unique())))
+        X_train = X_train.drop(columns = feature)
+    # st.write(X_train)
+    return X_train, Y_train
+
+# le = LabelEncoder()
+
+# X_trainC = X_train.select_dtypes(include=[object]).apply(le.fit_transform)
+# X_trainC['AgeB'] = X_train['AgeBand']
+
+# X_testC = test.select_dtypes(include=[object]).apply(le.fit_transform)
+# X_testC['AgeB'] = test['AgeBand']
+
+# st.dataframe(X_trainC,hide_index=True)
+# st.dataframe(X_testC,hide_index=True)
+load_data()
+st.write("Model train")
+
+st.session_state['pModel'] = st.button("Preapare Model",)
+if st.session_state['pModel'] == True:
+    models = {
+        "SVM" : {'model' : SVC(), 'kernel' : ['linear', 'rbf', 'poly', 'sigmoid'] },
+        "DT" : DecisionTreeClassifier(),
+        "RF" : RandomForestClassifier(),
+        "NB" : GaussianNB()
+    }
+#  
+    X_train, Y_train = load_data()
+
+    # st.dataframe(X_train,)
+    model_name = list(models.keys())
+    # st.write(f"{model_name}:")
+    for model in model_name:
+        if model == "SVM":
+            scaler = StandardScaler()
+            for kernel in models[model]['kernel']:
+                clf = models[model]['model'].set_params(kernel=kernel)
+                # X_train = scaler.fit_transform(X_train)
+                clf.fit(X_train, Y_train)
+                score = cross_val_score(clf, X_train, Y_train, cv=5).mean()
+                st.write(f"{model} : {kernel}: {score}")
+                with open(f'./models/{model}_{kernel}.pkl', "wb") as f:
+                    pkl.dump(clf, f)
+
+        else:
+            clf = models[model].fit(X_train, Y_train)
+            score = cross_val_score(clf, X_train, Y_train, cv=5).mean()
+            st.write(f"{model}: {score}")
+            with open(f'./models/{model}.pkl', "wb") as f:
+                pkl.dump(clf, f)
+
+
+# st.button("Test Model", on_click=test_page)
+
---
+++ pages/test.py
@@ -0,0 +1,89 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle as pkl
+
+if 'model_loaded' not in st.session_state:
+    st.session_state['model_loaded'] = None
+
+def take_input(model=None):
+    if model is not None:
+        with st.form('Select Parameters to test:', clear_on_submit=True):
+            features = pkl.load(open(f'./models/Features.pkl', 'rb'))
+            # st.write(features)
+            keys = [x for x in features.keys()]
+            default_value = None
+            new_data = dict.fromkeys(keys, default_value)
+                # st.write(param)
+            for feature in features.keys():
+                options = features[feature].keys()
+                # st.write(options)
+                tempV1 = features[feature][st.selectbox(f"Select value for {feature[::-1][2:][::-1]}", 
+                        options = options)]
+                empL = []
+                empL.append(tempV1)
+                new_data[feature] = empL
+            
+            st.session_state['subB'] = st.form_submit_button('Submit')
+ 
+    if 'subB' in st.session_state and st.session_state['subB']:
+                # st.write(new_data)
+                # st.write(subB) 
+        # st.write(pd.DataFrame(new_data).reset_index())
+        y_pred=model.predict(pd.DataFrame(new_data,)) 
+        if y_pred[0] == 0:
+            st.subheader('Predicted Class: Didn\'t Survived')
+        else:
+            st.subheader('Predicted Class: Survived')
+        # st.write(f'Predicted Class: {y_pred[0]}')      
+
+# @st.cache_resource
+def load_model(model, kernel):
+    try:
+        match model:
+            case 'Naive Bayesian':
+                st.session_state['model_loaded'] = pkl.load(open(f'./models/NB.pkl', 'rb'))
+            case 'Decesion Tree':
+                st.session_state['model_loaded'] = pkl.load(open(f'./models/DT.pkl', 'rb'))
+            case 'Random Forest':
+                st.session_state['model_loaded'] = pkl.load(open(f'./models/RF.pkl', 'rb'))
+            case 'SVM':
+                match kernel:
+                    case 'Linear':
+                        st.session_state['model_loaded']= pkl.load(open(f'./models/{model}_linear.pkl', 'rb'))
+                    case 'RBF':
+                        st.session_state['model_loaded'] = pkl.load(open(f'./models/{model}_rbf.pkl', 'rb'))
+                    case 'Polynomial':
+                        st.session_state['model_loaded'] = pkl.load(open(f'./models/{model}_poly.pkl', 'rb'))
+                    case 'Sigmoidal':
+                        st.session_state['model_loaded'] = pkl.load(open(f'./models/{model}_sigmoid.pkl', 'rb'))
+        # st.success(st.session_state['model_loaded'])
+        # st.success('Model loaded successfully!')
+        
+        
+    except FileNotFoundError:
+        st.error('Model not found. Please make sure the model file exists.')
+        return None
+
+if st.session_state['model_loaded'] is not None:
+    st.success(f'Model loaded successfully!: {st.session_state['model_loaded']}')
+    take_input(st.session_state['model_loaded'])  
+def test_page():
+    with st.sidebar:
+        st.title('Select Model to test')
+        model = st.selectbox('Select Models to test: ', options=['Naive Bayesian', 'Decesion Tree',
+                                    'Random Forest', 'SVM'])
+        if model == 'SVM':
+            kernel = st.selectbox('Select Kernel for SVM: ', options=['Linear', 'RBF', 'Polynomial', 'Sigmoidal'])
+        else:
+            kernel = 'None'
+
+        b1 = st.button('Load Model', on_click=load_model, args= (model, kernel))
+
+# take_input(st.session_state['model_loaded']) 
+test_page()
+
+
+
+
+    
