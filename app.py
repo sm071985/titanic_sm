09929e2dfa05d62b20e7b395c92d3d3ecb0d28f7
sm071985 <sm071985@gmail.com>
2025-02-18 23:51:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def cata2lbl(df, features):
         for value in values:
             fea_key = value.upper() if type(value) == str else value
             if feature+'_l' not in feature_dict.keys():
-                feature_dict[feature+'_l'] = {fea_key == value.upper(): values.index(value)}
+                feature_dict[feature+'_l'] = {fea_key : values.index(value)}
             else:
                 feature_dict[feature+'_l'].update({fea_key : values.index(value)})
             df.loc[(df[feature] == value), feature+'_l'] = values.index(value)
